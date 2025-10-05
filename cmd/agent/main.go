04ecada8e2b4d11ed6099f44437987bb7aea7736
Stepovoy Sergey <sastepo1@mts.ru>
2025-10-05 22:09:23 +0300

    iter20: init

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"fmt"
+
 	"go.uber.org/fx"
 
 	"github.com/dontagr/metric/internal/agent/bootstrap"
 )
 
+var buildVersion string
+var buildDate string
+var buildCommit string
+
 func main() {
+	if buildVersion == "" {
+		buildVersion = "N/A"
+	}
+	fmt.Printf("Build version: \"%s\"\n", buildVersion)
+	if buildDate == "" {
+		buildDate = "N/A"
+	}
+	fmt.Printf("Build date: \"%s\"\n", buildDate)
+	if buildCommit == "" {
+		buildCommit = "N/A"
+	}
+	fmt.Printf("Build commit: \"%s\"\n", buildCommit)
+
 	fx.New(CreateApp()).Run()
 }
 
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	_ "net/http/pprof"
 
 	"go.uber.org/fx"
@@ -8,7 +9,24 @@ import (
 	"github.com/dontagr/metric/internal/server/bootstrap"
 )
 
+var buildVersion string
+var buildDate string
+var buildCommit string
+
 func main() {
+	if buildVersion == "" {
+		buildVersion = "N/A"
+	}
+	fmt.Printf("Build version: \"%s\"\n", buildVersion)
+	if buildDate == "" {
+		buildDate = "N/A"
+	}
+	fmt.Printf("Build date: \"%s\"\n", buildDate)
+	if buildCommit == "" {
+		buildCommit = "N/A"
+	}
+	fmt.Printf("Build commit: \"%s\"\n", buildCommit)
+
 	fx.New(CreateApp()).Run()
 }
 
